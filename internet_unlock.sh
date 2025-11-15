05fb484d125663cd1b57effaeb9f3539fb0dbf2d
KAGmay05 <kelenalfarogarcia@gmail.com>
2025-11-15 16:33:33 -0500

    Main functionality implemented

---
+++ internet_unlock.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CLIENT_IP="$1"
+
+if[ -z "$CLIENT_IP" ]; then
+    echo "Uso: ./internet_unlock.sh <IP_DEL_CLIENTE>"
+    exit 1
+fi
+
+iptables -I FORWARD -s $CLIENT_IP -j ACCEPT
+iptables -I FORWARD -d $CLIENT_IP -j ACCEPT
+
+echo "Cliente $CLIENT_IP desbloqueado. Ya tiene acceso a Internet."
