0cab7d103465b7f3f5bf78454917f14ed5ac3a9f
abg555 <boueadriana2@gmail.com>
2025-11-18 14:56:24 -0500

    IP masquerade
    IP masquerade

--- internet_block.sh
+++ internet_block.sh
@@ -8,7 +8,7 @@ iptables -F
 iptables -t nat -F
 
 echo 1 > /proc/sys/net/ipv4/ip_forward
-
+iptables -t nat -A POSTROUTING -o $INT -j MASQUERADE
 iptables -A FORWARD -i $INT -j DROP
 iptables -A INPUT -p tcp --dport 8000 -j ACCEPT
 iptables -t nat -A PREROUTING -i $INT -p tcp --dport 8000 -j DNAT --to-destination $SERVER_IP:8000
